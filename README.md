a5881de82ef1d413be28a202e76b78af7b034b00
liubo <liubo@beigebigdata.com>
2020-10-16 18:28:12 +0800

    增加readme.md

---
+++ README.md
@@ -0,0 +1,23 @@
+# 项目说明
+- node版本v12.18.4
+- —| config文件是webpack配置
+- —| —| constant  配置
+- —| src文件项目代码
+- —| —| pages页面
+- 使用cssmodule写css，参考src/components/App组件
+
+# 启动
+- npm i
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from '@/components/APP/App'
-import "lib-flexible"
-import 'antd-mobile/dist/antd-mobile.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '@/components/APP/App';
+import 'lib-flexible';
 
 if (module && module.hot) {
-    module.hot.accept()
+    module.hot.accept();
 }
 
-console.log("配置的常量",process.NODE_ENV)
+console.log('配置的常量', process.NODE_ENV);
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+ReactDOM.render(<App />, document.querySelector('#root'));
