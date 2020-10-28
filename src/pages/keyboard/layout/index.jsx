c5d3c116765867aa4e7c9f2d235cbca2c49cf476
zhengjian <1838915268@qq.com>
2020-10-28 18:03:11 +0800

    加个表单校验库@alifd/field

--- src/pages/keyboard/layout/index.jsx
+++ src/pages/keyboard/layout/index.jsx
@@ -3,22 +3,31 @@ import style from './index.css';
 import WrapLayout from '@/components/wrapLayout/index';
 import ErrorBoundary from '@/components/errorBoundary/index';
 import { InputItem } from 'antd-mobile';
+import Field from '@alifd/field';
 
 @ErrorBoundary
 @WrapLayout
 export default class Layout extends React.Component {
+    field = new Field(this); // 实例创建
+
+    submit = () => {
+        const { getValues, getErrors } = this.field;
+        console.log(getValues(), getErrors());
+    };
     render() {
+        const { init, getError } = this.field;
         return (
             <div className={style.wrap}>
                 <InputItem
                     type="text"
                     placeholder="请输入标的代码"
                     clear
-                    needScroll={'false'}
-                    onChange={() => {}}
-                    onFocus={() => {}}
-                    onBlur={() => {}}
+                    {...init('name', {
+                        rules: [{ required: true }],
+                        initValue: 'first value',
+                    })}
                 />
+                {getError('name') && <div style={{ color: 'red' }}>{getError('name')}</div>}
 
                 <InputItem
                     type="phone"
@@ -63,9 +72,9 @@ export default class Layout extends React.Component {
                     autoAdjustHeight={true}
                     moneyKeyboardAlign={'left'}
                     placeholder="请输入数字1111"
-                    onChange={() => {}}
-                    onFocus={() => {}}
-                    onBlur={() => {}}
+                    {...init('money1', {
+                        rules: [{ required: true }, { minLength: 4 }],
+                    })}
                     extra={'%'}
                 />
 
@@ -74,12 +83,14 @@ export default class Layout extends React.Component {
                     autoAdjustHeight={true}
                     moneyKeyboardAlign={'left'}
                     placeholder="请输入数字222"
-                    onChange={() => {}}
-                    onFocus={() => {}}
-                    onBlur={() => {}}
+                    {...init('money2', {
+                        rules: [{ required: true }, { minLength: 4 }],
+                    })}
                     extra={'%'}
                 />
-                <div className={style.footer}>提交</div>
+                <div className={style.footer} onClick={this.submit}>
+                    提交
+                </div>
             </div>
         );
     }
